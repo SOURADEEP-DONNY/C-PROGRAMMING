bc7d0bc3afcbe03185428dc5fdb3a8363a497093
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-14 23:17:39 +0530

    Add files via upload

---
+++ PATTERN DESIGNS/2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	//HERE 'i' REPRESENTS THE ROWS.
+	//HERE 'j' REPRESENTS THE COLUMNS
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+printf("\n");
+}
