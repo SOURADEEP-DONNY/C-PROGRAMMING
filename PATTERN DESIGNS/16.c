9e60e22f4debf1d0a7ec95a122516d0c44ff303e
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-20 17:22:37 +0530

    Add files via upload

---
+++ PATTERN DESIGNS/16.c
@@ -0,0 +1,22 @@
+#include<conio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=7;j++)
+		{
+			if(j>=i&&j<=8-i)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	printf("\n");
+	}
+printf("\n");
+return 0;
+}
