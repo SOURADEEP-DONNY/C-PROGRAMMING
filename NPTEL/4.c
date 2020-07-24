e063870b0cdf197ceb7ca20bb5c82e60fe2733d6
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-24 15:47:09 +0530

    Add files via upload

---
+++ NPTEL/4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	float centrigrades;
+	float fahrenheit;
+	centrigrades=50;
+	fahrenheit=((9*centrigrades)/5)+32;
+	printf("\nThe temperature %f Celcius",centrigrades);
+	printf(" equals to %f fahrenheit\n",fahrenheit);
+}
