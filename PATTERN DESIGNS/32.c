a745e5add325476ad59a46f32f508c2ad811bc1b
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-28 20:48:58 +0530

    Add files via upload

---
+++ PATTERN DESIGNS/32.c
@@ -0,0 +1,23 @@
+// HOW TO PRINT A SQAURE IN C
+//==============================================================================
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=7;j++)
+		{
+			if(i==1||i==7||j==1||j==7)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	printf("\n");
+	}
+printf("\n");
+}
