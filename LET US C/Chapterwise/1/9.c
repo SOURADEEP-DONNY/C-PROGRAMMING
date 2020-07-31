f962c8172ba49b4223e59ed6c8dfe069ad3d6a2d
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-08-01 01:30:50 +0530

    Add files via upload

---
+++ LET US C/Chapterwise/1/9.c
@@ -0,0 +1,22 @@
+//==============================================================================
+/* If a number is input through the keyboard, write a
+   program to calculate the sum of its digits.
+*/
+//==============================================================================
+#include<stdio.h>
+void main()
+{
+	int n,s=0,r;
+	printf("\nEnter a number:-");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		s=s*10+r;
+		n=n/10;
+	}
+	printf("Reverse = %d",s);
+	printf("\n");
+	
+}
+//==============================================================================
