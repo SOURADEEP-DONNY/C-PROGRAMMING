50e3296ae52de5ca63861326e41ba5f211838b53
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-08-04 22:18:11 +0530

    Add files via upload

---
+++ Technical  Questions/questions.c
@@ -0,0 +1,101 @@
+/*==================================================================================================
+	What are the types of linkages?
+
+	A.	Internal and External
+	B.	External, Internal and None
+	C.	External and None
+	D.	Internal
+	Answer: Option B
+
+	Explanation:
+
+	External Linkage-> means global, non-static variables and functions.
+	Internal Linkage-> means static variables and functions with file scope.
+	None Linkage-> means Local variables.
+=====================================================================================================
+
+	Which of the following special symbol allowed in a variable name?
+
+	A.	* (asterisk)
+	B.	| (pipeline)
+	C.	- (hyphen)
+	D.	_ (underscore)	
+	Answer: Option D
+
+	Explanation:
+
+	Variable names in C are made up of letters (upper and lower case) and digits. 
+	The underscore character ("_") is also permitted.
+	Names must not begin with a digit.
+======================================================================================================
+	
+	 How would you round off a value from 1.66 to 2.0?
+
+	A.	ceil(1.66)	
+	B.	floor(1.66)
+	C.	roundup(1.66)
+	D.	roundto(1.66)
+	
+	Answer: Option A
+======================================================================================================
+	By default a real number is treated as a
+
+	A.	float
+	B.	double	
+	C.	long double
+	D.	far double
+	
+	Answer: Option B
+========================================================================================================
+	
+	Which of the following is not user defined data type?
+	1 :	
+	struct book
+	{
+	    char name[10];
+	    float price;
+	    int pages;
+	};
+	2 :	
+	long int l = 2.35;
+	3 :	
+	enum day {Sun, Mon, Tue, Wed};
+	
+	A.	1
+	B.	2	
+	C.	3
+	D.	Both 1 and 2
+	Answer: Option B
+	
+	Explanation:
+	
+	C data types classification are
+	
+	Primary data types
+	int
+	char
+	float
+	double
+	void
+	Secondary data types (or) User-defined data type
+	Array
+	Pointer
+	Structure
+	Union
+	Enum
+====================================================================================
+
+	Identify which of the following are declarations
+	1 :	extern int x;
+	2 :	float square ( float x ) { ... }
+	3 :	double pow(double, double);
+	
+	A.	1
+	B.	2
+	C.	1 and 3	
+	D.	3
+	
+	Answer: Option C
+====================================================================================
+
+
