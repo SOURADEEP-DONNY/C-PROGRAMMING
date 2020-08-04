4790d223c001a05681d58b196c9127ac788fd3e6
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-08-04 21:19:40 +0530

    Add files via upload

---
+++ Technical  Questions/2.c
@@ -0,0 +1,22 @@
+/* Example for ceil() and floor() functions: */
+//======================================================================
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("\n Result : %f" , ceil(1.44) );
+    printf("\n Result : %f" , ceil(1.66) );
+ 
+    printf("\n Result : %f" , floor(1.44) );    
+    printf("\n Result : %f" , floor(1.66) );
+
+    return 0;
+}
+//========================================================================
+// Output:
+// Result : 2.000000
+// Result : 2.000000
+// Result : 1.000000
+// Result : 1.000000
+//==========================================================================
