ea20bd0c46b4b153a654d80702ba89d12a2542cc
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-08-25 22:41:29 +0530

    Add files via upload

---
+++ Technical  Questions/15.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=4;
+	switch(i)
+	{
+		default:
+			printf("Oracle\n");
+		case 1:
+			printf("C\n");
+			break;
+		case 2:
+			printf("C++\n");
+			break;
+		case 3:
+			printf("JAVA\n");
+	}
+}
